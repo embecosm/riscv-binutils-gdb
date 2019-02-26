1b4a52f55f1818268028eb65fdeb1d489ecbbf84
Paulo Matos <pmatos@linki.tools>
2019-02-26 13:40:24 +0100

    Revert "Add c.addiw hint and regenerate"
    This reverts commit d041152368f5a2fbcfeeddfeaf8d8f348d6aac1f.

--- opcodes/riscv-opc.c
+++ opcodes/riscv-opc.c
@@ -2447,10 +2447,6 @@ static const CGEN_IFMT ifmt_p_c_slli_hint ATTRIBUTE_UNUSED = {
   16, 16, 0xe003, { { F (F_C_FUNCT3) }, { F (F_UIMM6_121_65) }, { F (F_UIMM5_115) }, { F (F_C_OPCODE) }, { 0 } }
 };
 
-static const CGEN_IFMT ifmt_p_c_addiw_hint ATTRIBUTE_UNUSED = {
-  16, 16, 0xe003, { { F (F_C_FUNCT3) }, { F (F_IMM6_121_65) }, { F (F_UIMM5_115) }, { F (F_C_OPCODE) }, { 0 } }
-};
-
 static const CGEN_IFMT ifmt_p_c_nop ATTRIBUTE_UNUSED = {
   16, 16, 0xffff, { { F (F_UIMM16_1516) }, { 0 } }
 };
@@ -3371,11 +3367,6 @@ static const CGEN_IBASE riscv_cgen_macro_insn_table[] =
 /* c.slli ${c-reg117-0},${nzuimm6-121-65-abs} */
   {
     -1, "p-c-slli-hint", "c.slli", 16,
-    { 0|A(ALIAS), { { { (1<<MACH_BASE), 0 } }, { { 1, "\xc0" } }, { { (1<<RVEXT_RV32C)|(1<<RVEXT_RV64C), 0 } } } }
-  },
-/* c.addiw ${c-reg117-0},${imm6-121-65-abs} */
-  {
-    -1, "p-c-addiw-hint", "c.addiw", 16,
     { 0|A(ALIAS), { { { (1<<MACH_BASE), 0 } }, { { 1, "\xc0" } }, { { (1<<RVEXT_RV32C)|(1<<RVEXT_RV64C), 0 } } } }
   },
 /* nop */
@@ -4518,12 +4509,6 @@ static const CGEN_OPCODE riscv_cgen_macro_insn_opcode_table[] =
     { 0, 0, 0, 0 },
     { { MNEM, ' ', OP (C_REG117_0), ',', OP (NZUIMM6_121_65_ABS), 0 } },
     & ifmt_p_c_slli_hint, { 0x2 }
-  },
-/* c.addiw ${c-reg117-0},${imm6-121-65-abs} */
-  {
-    { 0, 0, 0, 0 },
-    { { MNEM, ' ', OP (C_REG117_0), ',', OP (IMM6_121_65_ABS), 0 } },
-    & ifmt_p_c_addiw_hint, { 0x2001 }
   },
 /* nop */
   {
