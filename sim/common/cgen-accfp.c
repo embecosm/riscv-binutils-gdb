0360d090c54f50be930954d7d0dd4fb4e420f64d
Edward Jones <ed.jones@embecosm.com>
2019-09-05 10:59:01 +0100

    sim/riscv: Add ufloatsidf to fix fcvt.d.wu semantics

--- sim/common/cgen-accfp.c
+++ sim/common/cgen-accfp.c
@@ -359,6 +359,17 @@ ufloatsisf (CGEN_FPU* fpu, int how UNUSED, USI x)
   return res;
 }
 
+static DF
+ufloatsidf (CGEN_FPU* fpu, int how UNUSED, USI x)
+{
+  sim_fpu ans;
+  unsigned64 res;
+
+  sim_fpu_u32to (&ans, x, sim_fpu_round_near);
+  sim_fpu_to64 (&res, &ans);
+  return res;
+}
+
 static SI
 fixsfsi (CGEN_FPU* fpu, int how UNUSED, SF x)
 {
@@ -761,6 +772,7 @@ cgen_init_accurate_fpu (SIM_CPU* cpu, CGEN_FPU* fpu, CGEN_FPU_ERROR_FN* error)
   o->floatsisf = floatsisf;
   o->floatsidf = floatsidf;
   o->ufloatsisf = ufloatsisf;
+  o->ufloatsidf = ufloatsidf;
   o->fixsfsi = fixsfsi;
   o->fixdfsi = fixdfsi;
   o->ufixsfsi = ufixsfsi;
